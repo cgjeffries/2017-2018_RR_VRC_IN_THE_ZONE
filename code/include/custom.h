765946115e965e75ec84587f568dce894325a0be
sazrocks <sazrocks6@gmail.com>
2017-06-13 13:33:02 -0700

    thing

--- code/include/custom.h
+++ code/include/custom.h
@@ -11,7 +11,8 @@
 #define QUAD2 2
 
 //PID structs
-#ifndef NAME
+#ifndef TEST0
+#define TEST0
 typedef struct NAME{
   double Kp;
   double Ki;
@@ -23,18 +24,15 @@ typedef struct NAME{
   double target;
   double sensor;
 }PID;
-#endif
-#ifndef BL
+
 PID BL = {
   .Kp = 0, .Ki = 0, .Kd = 0, .error = 0, .previous_error = 0, .integral = 0, .derivative = 0, .target = 0, .sensor = 0
 };
-#endif
-#ifndef BR
+
 PID BR = {
   .Kp = 0, .Ki = 0, .Kd = 0, .error = 0, .previous_error = 0, .integral = 0, .derivative = 0, .target = 0, .sensor = 0
 };
-#endif
-#ifndef T
+
 PID T = {
   .Kp = 0, .Ki = 0, .Kd = 0, .error = 0, .previous_error = 0, .integral = 0, .derivative = 0, .target = 0, .sensor = 0
 };
--- code/include/main.h
+++ code/include/main.h
@@ -44,7 +44,7 @@
 #define MAIN_H_
 
 #include <API.h>
-#include <custom.h>
+
 
 
 // Allow usage of this file in C++ programs
@@ -53,6 +53,7 @@ extern "C" {
 #endif
 
 Encoder mainQuad;
+#include <custom.h>
 
 //#define AUTO_DEBUG
 
